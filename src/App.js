247900ca845f8fe8e4e4eb929fec632bd800d722
xovest <79008917+xovest@users.noreply.github.com>
2021-11-12 16:50:22 +0200

    currencies update cause of onchange

--- src/App.js
+++ src/App.js
@@ -23,9 +23,17 @@ function App() {
   return (
     <>
       <h1>Convert</h1>
-      <CurrencyRow currencyOptions={currencyOptions} />
+      <CurrencyRow 
+        currencyOptions={currencyOptions} 
+        selectedCurrency={fromCurrency}
+        onChangeCurrency={e => setFromCurrency(e.target.value)}
+      />
       <div className="equals">=</div>
-      <CurrencyRow currencyOptions={currencyOptions} />
+      <CurrencyRow 
+        currencyOptions={currencyOptions} 
+        selectedCurrency={toCurrency}
+        onChangeCurrency={e => setToCurrency(e.target.value)}
+      />
     </>
   );
 }
