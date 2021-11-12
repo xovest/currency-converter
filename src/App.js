297ad3b3a0c66f23f3a2dcc92f346934c8d858f0
xovest <79008917+xovest@users.noreply.github.com>
2021-11-12 16:46:45 +0200

    currencies added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import CurrencyRow from './CurrencyRow';
+
+const BASE_URL = 'http://api.exchangeratesapi.io/v1/latest?access_key=f798e5b8808f0895d825ddf3cd935580&format=1';
 
 function App() {
+  const [currencyOptions, setCurrencyOptions] = useState([]);
+  const [fromCurrency, setFromCurrency] = useState();
+  const [toCurrency, setToCurrency] = useState();
+
+  useEffect(() => {
+    fetch(BASE_URL)
+      .then(res => res.json())
+      .then(data => {
+        const firstCurrency = Object.keys(data.rates)[0];
+        setCurrencyOptions([data.base, ...Object.keys(data.rates)]);
+        setFromCurrency(data.base);
+        setToCurrency(firstCurrency)
+      });
+  }, []);
+
   return (
-    <h1>I suck</h1>
+    <>
+      <h1>Convert</h1>
+      <CurrencyRow currencyOptions={currencyOptions} />
+      <div className="equals">=</div>
+      <CurrencyRow currencyOptions={currencyOptions} />
+    </>
   );
 }
 
